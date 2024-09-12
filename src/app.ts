1f63f8a7573976778af33c948799d71b8f06757d
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-09-12 13:56:37 -0300

    Criando Schema do Prisma

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,10 @@ import fastify from 'fastify'
 export const app = fastify()
 
 const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Fulano da Silva',
+    email: 'Fulano@usuario.com.br',
+  },
+})
