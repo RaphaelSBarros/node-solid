5a0b1ef97c6699420fc851e85c9c08795af0e47a
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-09-20 09:32:55 -0300

    Testes de RBAC

--- src/http/controller/gyms/search.spec.ts
+++ src/http/controller/gyms/search.spec.ts
@@ -13,7 +13,7 @@ describe('Search Gyms (e2e)', () => {
   })
 
   it('should be able to search gyms by title', async () => {
-    const { token } = await createAndAuthenticate(app)
+    const { token } = await createAndAuthenticate(app, true)
 
     await request(app.server)
       .post('/gyms')
